7ee97a006d8ae4a521e167d2936bde39a5f6a2e7
Nadil-Dulran <it23161788@my.sliit.lk>
2025-11-07 00:34:09 +0530

    Develop Profile page

--- client/src/pages/Profilepage.jsx
+++ client/src/pages/Profilepage.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import assets from '../assets/assets'
 
-function Profilepage() {
+const Profilepage = () => {
+
+  const [selectedImg, setSelectedImg] = React.useState(null);
+  const navigate = useNavigate();
+  const[name, setName] = React.useState("Martin Johnson");
+  const[bio, setBio] = React.useState("Hi Everyone, I am Using RahasChat");
+
+  const handleSubmit = async (e)=> {
+    e.preventDefault();
+    navigate('/');
+  }
+
   return (
-    <div>
-      <h1>Profile Page</h1>
+    <div className='min-h-screen bg-cover bg-no-repeat flex items-center justify-center'>
+      <div className='w-5/6 max-w-2xl backdrop-blur-2xl text-gray-400 border-2 border-gray-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg'>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-5 p-10 flex-1">
+          <h3 className="text-lg">Profile Information</h3>
+          <label htmlFor="avatar" className='flex items-center gap-3 cursor-pointer'>
+            <input onChange={(e)=>setSelectedImg(e.target.files[0])} type="file" id="avatar" accept='.png, .jpg, .jpeg' hidden/>
+            <img src={selectedImg ? URL.createObjectURL(selectedImg) : assets.avatar_icon} alt="" className={`w-12 h-12 ${selectedImg && 'rounded-full'}`}/>
+            Upload profile picture
+            </label>
+            <input onChange={(e)=>setName(e.target.value)} value={name} type="text" required placeholder='Your name' className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500' />
+            <textarea onChange={(e)=>setBio(e.target.value)} value={bio} placeholder="Write your bio..." required className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500' rows={4}></textarea>
+            <button type="Submit" className='py-3 bg-linear-to-r from-purple-500 to-violet-700 text-white rounded-md cursor-pointer'>Save</button>
+        </form>
+        <img className={'max-w-44 aspect-square mx-10 max-sm:mt-10'} src={assets.logo_icon} alt="" />
+      </div>
+
     </div>
   )
 }
