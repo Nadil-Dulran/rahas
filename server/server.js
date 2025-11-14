198b9c93653f00211a0a54e536b6358b0daa053f
Nadil-Dulran <it23161788@my.sliit.lk>
2025-11-14 19:05:42 +0530

    Created basic server

---
+++ server/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import http from "http";
+
+// Create Express app and HTTP server
+
+const app = express();
+const server = http.createServer(app);
+
+// Middleware
+app.use(cors());
+app.use(express.json({ limit: "4mb" }));
+
+// Sample route
+app.use("/api/status", (req, res) => res.send("Server is running..."));
+
+// Start server
+const PORT = process.env.PORT || 5001;
+server.listen(PORT, () => console.log("Server is running on port: " + PORT));
