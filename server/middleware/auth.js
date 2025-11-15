dbb4f6e1a5ba2fa774e7492b5279703b88502fa4
Nadil-Dulran <it23161788@my.sliit.lk>
2025-11-15 18:10:03 +0530

    Developed middleware for Handle users

---
+++ server/middleware/auth.js
@@ -0,0 +1,25 @@
+// Function to authenticate user using JWT execute before executing the controller functions
+
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+
+// Middleware to protect routes
+export const protectRoute = async (req, res, next) => {
+    try{
+        const token = req.headers.token;
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        const user = await User.findById(decoded.userID).select("-password");
+
+        if(!user)  return res.json({ success: false, message: "Unauthorized Access" });
+        
+        req.user = user;
+        next();
+
+    } catch(error){
+        console.log(error.message);
+        res.json({ success: false, message: "Unauthorized Access" });   
+    }
+
+}
