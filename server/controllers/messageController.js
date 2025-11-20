ca56c65455161e1894fae915991183e36f7aa15e
Nadil-Dulran <it23161788@my.sliit.lk>
2025-11-20 20:28:13 +0530

    Developed method for get the messages of a user

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -25,5 +25,29 @@ export const getUsersForSidebar = async (req, res) => {
     }
 
     // Get all messages for selected chat user
+    export const getMessages = async (req, res) => {
+        try {
+            const { id: selectedUserId } = req.params;
+            const myId = req.user._id;
 
-    
+            const messages = await Message.find({
+                $or: [
+                    { senderId: myId, receiverId: selectedUserId },
+                    { senderId: selectedUserId, receiverId: myId }
+                ]
+            })
+            await Message.updateMany(
+                { senderId: selectedUserId, receiverId: myId},
+                { seen: true  });
+
+            res.json({ success: true, messages }
+            );
+        } catch (error) {
+            console.log(error.message);
+            res.json({ success: false, message: error.message });
+        }
+
+
+    }
+
+//
