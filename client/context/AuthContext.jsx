9c933d31da245f0079560fe6fcdd146a71ea297e
Nadil-Dulran <it23161788@my.sliit.lk>
2025-11-21 14:14:17 +0530

    Develop function to check whether user authenticate

--- client/context/AuthContext.jsx
+++ client/context/AuthContext.jsx
@@ -14,6 +14,19 @@ export const AuthProvider = ({ children }) => {
     const [onlineUsers, setOnlineUsers] = useState([]);
     const [socket, setSocket] = useState(null);
 
+// Check if user is authenticated and if, set the user data and connect socket
+const checkAuth = async () => {
+        try {
+            const {data} = await axios.get("/api/auth/check");
+            if (data.success) {
+                setAuthUser(data.user);
+                // Initialize socket connection here if needed
+            }
+        } catch (error) {
+            console.error("Authentication check failed", error);
+        }
+    }
+
 
     const value = {
         axios,
