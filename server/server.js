9b68f15c8524c4f875a6d5c64c92c11e054e09a8
Nadil-Dulran <it23161788@my.sliit.lk>
2025-11-23 16:22:16 +0530

    Update server.js for Vercel

--- server/server.js
+++ server/server.js
@@ -68,4 +68,11 @@ await connectDB();
 
 // Start server
 const PORT = process.env.PORT || 5001;
+
+if (!process.env.NODE_ENV !== "production") {
+
 server.listen(PORT, () => console.log("Server is running on port: " + PORT));
+}
+
+// Export server for Vercel
+export default server;
