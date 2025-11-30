78dc91e5076f139d33066837877a6538ba0b6462
Nadil-Dulran <it23161788@my.sliit.lk>
2025-11-30 11:42:02 +0530

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.js";
+import checkDomain from "../middleware/checkDomain.js"; 
 import { checkAuth, login, signup, updateProfile, getUsers } from "../controllers/userController.js";
 
 const userRoutes = express.Router();
 
-userRoutes.post("/signup", signup);
+userRoutes.post("/signup", checkDomain, signup);
 userRoutes.post("/login", login);
 userRoutes.put("/updateProfilePicture", protectRoute, updateProfile);
 userRoutes.get("/checkAuth", protectRoute, checkAuth);
